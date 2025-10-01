f2a7ce618de0848162ad5b92dc3ffa0a21fd2e1c
QuanggCK <zaphkiel1610@gmail.com>
2025-10-01 14:27:48 +0700

    update

--- BasicGUI_SimpleMath/app/build.gradle.kts
+++ BasicGUI_SimpleMath/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "clc65.quanggck.basicgui_simplemath"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "clc65.quanggck.basicgui_simplemath"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- BasicGUI_SimpleMath/app/src/main/java/clc65/quanggck/basicgui_simplemath/MainActivity.java
+++ BasicGUI_SimpleMath/app/src/main/java/clc65/quanggck/basicgui_simplemath/MainActivity.java
@@ -7,18 +7,72 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edt_numbera, edt_numberb;
+    TextView tv_result;
+    Button btn_plus, btn_minus, btn_multiply, btn_divide, btn_calculate;
+
+    String phepToan = ""; ////Chuỗi để lưu loại phép toán
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        edt_numbera = findViewById(R.id.edt_numbera);
+        edt_numberb = findViewById(R.id.edt_numberb);
+        tv_result = findViewById(R.id.tv_result);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_multiply = findViewById(R.id.btn_multiply);
+        btn_divide = findViewById(R.id.btn_divide);
+        btn_calculate = findViewById(R.id.btn_calculate);
+
+
+        ///  Gắn sự kiện chọn phép toán
+        btn_plus.setOnClickListener(v -> phepToan = "+");
+        btn_minus.setOnClickListener(v -> phepToan = "-");
+        btn_multiply.setOnClickListener(v -> phepToan = "*");
+        btn_divide.setOnClickListener(v -> phepToan = "/");
+
+
+        /// Gắn sự kiện tính toán
+        btn_calculate.setOnClickListener(v -> Tinh());
+    }
+    private void Tinh() {
+        try {
+            double a = Double.parseDouble(edt_numbera.getText().toString());
+            double b = Double.parseDouble(edt_numberb.getText().toString());
+            double kq = 0;
+
+            switch (phepToan) {
+                case "+":
+                    kq = a + b;
+                    break;
+                case "-":
+                    kq = a - b;
+                    break;
+                case "*":
+                    kq = a * b;
+                    break;
+                case "/":
+                    if (b != 0) kq = a / b;
+                    else tv_result.setText("Cannot divide by 0");
+                    return;
+                default:
+                    tv_result.setText("Haven't chosen the operation");
+                    return;
+            }
+            tv_result.setText(String.valueOf(kq));
+        } catch (Exception e) {
+            tv_result.setText("NaN");
+        }
     }
 }
