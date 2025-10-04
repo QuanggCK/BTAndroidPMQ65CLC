0f82bacccb50dbb1d53417dbbed673c90382e232
QuanggCK <zaphkiel1610@gmail.com>
2025-10-04 21:15:18 +0700

    first commit

--- LoginFigmaScreen/app/src/main/java/clc65/quanggck/loginfigmascreen/MainActivity.java
+++ LoginFigmaScreen/app/src/main/java/clc65/quanggck/loginfigmascreen/MainActivity.java
@@ -1,6 +1,9 @@
 package clc65.quanggck.loginfigmascreen;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EditText email = findViewById(R.id.edt_email);
+        Drawable icon = getResources().getDrawable(R.mipmap.email_icon);
+        icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
+        email.setCompoundDrawables(icon, null, null, null);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
 }
