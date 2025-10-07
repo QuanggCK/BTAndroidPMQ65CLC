7cc2bf663884741387dbd57da93736a1620b8172
QuanggCK <zaphkiel1610@gmail.com>
2025-10-07 22:04:11 +0700

    first commit + update

--- lamthem_cardInfo/app/src/main/java/clc65/quanggck/lamthem_cardinfo/MainActivity.java
+++ lamthem_cardInfo/app/src/main/java/clc65/quanggck/lamthem_cardinfo/MainActivity.java
@@ -1,6 +1,8 @@
 package clc65.quanggck.lamthem_cardinfo;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView = findViewById(R.id.textView11);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        
+
+
     }
 }
