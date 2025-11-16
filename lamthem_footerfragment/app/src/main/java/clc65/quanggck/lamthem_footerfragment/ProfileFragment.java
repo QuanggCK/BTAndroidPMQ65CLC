a2d184c1b246f7b4465b9e382a831d310f4ec890
QuanggCK <zaphkiel1610@gmail.com>
2025-11-16 23:30:30 +0700

    update + finish

--- lamthem_footerfragment/app/src/main/java/clc65/quanggck/lamthem_footerfragment/ProfileFragment.java
+++ lamthem_footerfragment/app/src/main/java/clc65/quanggck/lamthem_footerfragment/ProfileFragment.java
@@ -1,24 +1,18 @@
 package clc65.quanggck.lamthem_footerfragment;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
-
-    public HomeFragment() {}
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
+public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate layout của fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
