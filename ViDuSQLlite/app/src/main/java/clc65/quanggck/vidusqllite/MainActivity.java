6141721384bf184725c3154602e5e30e0a540668
QuanggCK <zaphkiel1610@gmail.com>
2025-11-24 16:16:47 +0700

    finish episode 1 of 3

--- ViDuSQLlite/app/src/main/java/clc65/quanggck/vidusqllite/MainActivity.java
+++ ViDuSQLlite/app/src/main/java/clc65/quanggck/vidusqllite/MainActivity.java
@@ -18,13 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create CSDL
-        db = openOrCreateDatabase("books", MODE_PRIVATE, null);
-        // Create table book
-        String sqlXoaBang = "DROP TABLE IF EXISTS BOOKS";
-        String sqlTaoBang = "CREATE TABLE BOOKS(BookID integer PRIMARY KEY, BookName text, Page integer, Price Float, Description text)";
-
-        db.execSQL(sqlXoaBang);
-        db.execSQL(sqlTaoBang);
+        db = openOrCreateDatabase("books.db", MODE_PRIVATE, null);
+        //Because these lines above we just have to one-time code, we should not do this
+//        // Create table book
+//        String sqlXoaBang = "DROP TABLE IF EXISTS BOOKS";
+//        String sqlTaoBang = "CREATE TABLE BOOKS(BookID integer PRIMARY KEY, BookName text, Page integer, Price Float, Description text)";
+//
+//        db.execSQL(sqlXoaBang);
+//        db.execSQL(sqlTaoBang);
+//        // Add some datas
+//        String sqlThem = " INSERT INTO BOOKS VALUES (1,'Shinja Zero',315,105000,'Isekai with Tatasuki Makoto') ";
+//        String sqlThem2 = " INSERT INTO BOOKS VALUES (2, 'Shinja Zero 2',320,100000,'Isekai with Tatasuki Makoto') ";
+//        db.execSQL(sqlThem);
+//        db.execSQL(sqlThem2);
+//        /// Keep in mind that we can upload the outside sqllite from DB Browser to this simulator.
+//        /// We can do it in both ways (coding or just input directly in the table).
 
         // Test
         db.close();
