41e8433c7736065a36bd968e8da9f29b7793bdbb
QuanggCK <zaphkiel1610@gmail.com>
2025-11-24 15:58:11 +0700

    update

--- ViDuSQLlite/app/src/main/java/clc65/quanggck/vidusqllite/MainActivity.java
+++ ViDuSQLlite/app/src/main/java/clc65/quanggck/vidusqllite/MainActivity.java
@@ -1,5 +1,6 @@
 package clc65.quanggck.vidusqllite;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,15 +11,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Create CSDL
+        db = openOrCreateDatabase("books", MODE_PRIVATE, null);
+        // Create table book
+        String sqlXoaBang = "DROP TABLE IF EXISTS BOOKS";
+        String sqlTaoBang = "CREATE TABLE BOOKS(BookID integer PRIMARY KEY, BookName text, Page integer, Price Float, Description text)";
+
+        db.execSQL(sqlXoaBang);
+        db.execSQL(sqlTaoBang);
+
+        // Test
+        db.close();
+
     }
 }
